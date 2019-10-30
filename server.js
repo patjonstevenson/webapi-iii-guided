7f6e409871b0ae6efb464b7dedf7da1757544db4
Patrick Stevenson <pat.jon.stevenson@gmail.com>
2019-10-30 10:29:17 -0700

    set up gatekeeper middleware

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express'); // importing a CommonJS module
 const helmet = require("helmet");
+const morgan = require("morgan");
 
 const hubsRouter = require('./hubs/hubs-router.js');
 
@@ -7,16 +8,28 @@ const server = express();
 
 // Custom Middleware functions
 const requestLogger = (req, res, next) => {
-  console.log(`${new Date().toISOString()} ${req.method} request made to ${req.url}`);
-
+  console.log(`[${new Date().toISOString()}] ${req.method} to ${req.url}`);
   next();
 };
 
+const gateKeeper = (req, res, next) => {
+  // a new way of reading data sent by the client
+  const password = req.headers.password || "";
+  if (password && password.toLowerCase() === "mellon") {
+    next();
+  } else if (password) {
+    res.status(401).json({ you: "shall not pass", });
+  } else {
+    res.status(400).json({ you: "need to provide a password" });
+  }
+};
 
 // Global middleware
 server.use(helmet());
 server.use(express.json());
 server.use(requestLogger);
+server.use(morgan());
+server.use(gateKeeper);
 
 server.use('/api/hubs', hubsRouter);
 
