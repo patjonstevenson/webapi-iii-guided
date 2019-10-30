952e9d73bee92bc4e2fd769a89d3424bbb85882a
Patrick Stevenson <pat.jon.stevenson@gmail.com>
2019-10-30 10:00:12 -0700

    added requestLogger middleware function to log date, request method, and request url to console for each request made to the server

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 const express = require('express'); // importing a CommonJS module
+const helmet = require("helmet");
 
 const hubsRouter = require('./hubs/hubs-router.js');
 
 const server = express();
 
+// Custom Middleware functions
+const requestLogger = (req, res, next) => {
+  console.log(`${new Date().toISOString()} ${req.method} request made to ${req.url}`);
+
+  next();
+};
+
+
+// Global middleware
+server.use(helmet());
 server.use(express.json());
+server.use(requestLogger);
 
 server.use('/api/hubs', hubsRouter);
 
